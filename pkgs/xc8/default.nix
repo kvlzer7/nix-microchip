7bbdc5fa2c2c255a641e59a8b515ed7bb8ab6616
kvlzer7 <kazakinvlad@gmail.com>
2026-01-14 12:12:08 -0700

    rolled xc8 compiler version back

--- pkgs/xc8/default.nix
+++ pkgs/xc8/default.nix
@@ -1,5 +1,7 @@
 # FIXME: importing this from a level up doesn' work for some reason...
 pkgs : rec {
-   xc8_3_10 = pkgs.callPackage ./3.10.nix { };
-   xc8 = xc8_3_10; #i.e. default to latest
+   # xc8_3_10 = pkgs.callPackage ./3.10.nix { };
+   # xc8 = xc8_3_10; #i.e. default to latest
+   xc8_3_00 = pkgs.callPackage ./3.00.nix { };
+   xc8 = xc8_3_00; #i.e. default to latest
 }
