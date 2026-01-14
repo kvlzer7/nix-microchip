349e91495c29349dd14ca016e3ae0f95958e6553
kvlzer7 <kazakinvlad@gmail.com>
2026-01-13 22:41:35 -0700

    spelling mistake

--- pkgs/x8/3.10.nix
+++
@@ -1,4 +0,0 @@
-import ./common.nix {
-  version = "3.10";
-  hash = "sha256-628803b96f468a5981d6bc1d0a5e6c7fa809e4d87e3cca961805e2a857f5846e";
-}
--- pkgs/x8/default.nix
+++
@@ -1,5 +0,0 @@
-# FIXME: importing this from a level up doesn' work for some reason...
-pkgs : rec {
-   xc8_3_10 = pkgs.callPackage ./3.10.nix { };
-   xc8 = xc8_3_10; #i.e. default to latest
-}
